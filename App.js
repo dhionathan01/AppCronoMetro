1694a50e2af6c546b76ca8ec6ffbdf1e57d23ace
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2024-08-28 18:07:12 -0300

    build: :tada: iniciando projeto

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+class App extends Component {
+  render() { 
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+  },
+});
